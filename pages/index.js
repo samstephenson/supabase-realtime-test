4818af975b412bcb2dda348b7f1449fd8bc2d706
Sam Stephenson <sam@stephenson.net>
2023-03-27 18:13:15 +0100

    subscirbed to meetings

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+import { supabase } from "../utils/supabase";
 
 export default function Home() {
+  const [meetings, setMeetings] = useState([]);
+  const [localTranscript, setLocalTranscript] = useState("");
+  useEffect(() => {
+    supabase
+      .channel("any")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "meetings" },
+        payload => {
+          setMeetings(payload.new);
+          console.log("Change received!", payload);
+        }
+      )
+      .subscribe();
+  }, []);
+
+  useEffect(() => {
+    supabase
+      .from("meetings")
+      .update({ transcript_device: localTranscript })
+      .eq("id", "90ea83ff-2ca1-45bc-8beb-5df938903ee3")
+      .then(({ data, error }) => {
+        if (error) throw error;
+        console.log("Meeting updated!", data);
+      });
+  }, [localTranscript]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +40,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={"grid grid-cols-2 divide-y items-stretch"}>
+        <div className="p-4">
+          <h1 className="font-bold">Local Transript</h1>
+          <textarea
+            value={localTranscript}
+            onChange={e => setLocalTranscript(e.target.value)}
+            className="w-full h-64 border"
+            placeholder="Type transcript"
+          />
         </div>
+        <div className="p-4 overflow-y-scroll h-96">
+          <h1 className="font-bold">Supabase transcript</h1>
+          <p className="pb-16">{meetings.transcript_device}</p>
+        </div>
+        {/* <pre className="text-xs p-4">{JSON.stringify(meetings, null, 2)}</pre> */}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
