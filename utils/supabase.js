8308236e140fea383de9ba485c40a0703d9947be
Sam Stephenson <sam@stephenson.net>
2023-03-27 18:37:46 +0100

    move stuff to env

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wpezvjidxetkoklidmjx.supabase.co";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 export const supabase = createClient(
   SUPABASE_URL,
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwZXp2amlkeGV0a29rbGlkbWp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc4NDAzODUsImV4cCI6MTk5MzQxNjM4NX0.VradqnV3uMxX6aRFtPcF-i2nX_BO2IImKuPRCqrlBSM"
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
